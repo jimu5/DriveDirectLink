8c051d621ecf15f79f7f5d4a36d75aec8a2e93ad
Kimiato <38881232+Kimiato@users.noreply.github.com>
2020-04-27 21:22:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 $site_name = ""; //填入你的站点名，例如 https://214214.xyz/tools/dlink/
-$cf_workers = ""; //填入搭建的cf-worker链接,否则将默认使用作者所搭建的
+$cf_worker = ""; //填入搭建的cf-worker链接,如 https://gd.kway.workers.dev/, 否则将默认使用作者所搭建的
 ?>
